7b2ceb38f638946ba4506a35e43bc3b3089a0417
Mohammad Kiani <kianimohammad@outlook.com>
2020-01-10 17:37:45 -0500

    Overlays Polyline Polygon custom annotation

--- Overlay Demo/ViewController.swift
+++ Overlay Demo/ViewController.swift
@@ -7,14 +7,98 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let places = Place.getPlaces()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mapView.showsUserLocation = true
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        //        locationManager.startUpdatingLocation()
+        
+        addAnnotations()
+//        addPolyLine()
+        addPolygon()
     }
+    
+    func addAnnotations() {
+        mapView.delegate = self
+        mapView.addAnnotations(places)
+        
+        let overlays = places.map { (MKCircle(center: $0.coordinate, radius: 1000)) }
+        mapView.addOverlays(overlays)
+    }
+    
+    func addPolyLine() {
+        let locations = places.map {$0.coordinate}
+        let polyline = MKPolyline(coordinates: locations, count: locations.count)
+        mapView.addOverlay(polyline)
+    }
+    
+    func addPolygon() {
+        let locations = places.map{$0.coordinate}
+        let polygon = MKPolygon(coordinates: locations, count: locations.count)
+        mapView.addOverlay(polygon)
+    }
+    
+}
 
-
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        } else {
+            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotionView") ?? MKAnnotationView()
+            annotationView.image = UIImage(named: "ic_place")
+            annotationView.canShowCallout = true
+            annotationView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            return annotationView
+        }
+    }
+    
+    // this function is needed to add overlays
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+            let rendrer = MKCircleRenderer(overlay: overlay)
+            rendrer.fillColor = UIColor.black.withAlphaComponent(0.5)
+            rendrer.strokeColor = UIColor.green
+            rendrer.lineWidth = 2
+            return rendrer
+        } else if overlay is MKPolyline {
+            let rendrer = MKPolylineRenderer(overlay: overlay)
+            rendrer.strokeColor = UIColor.blue
+            rendrer.lineWidth = 3
+            return rendrer
+        } else if overlay is MKPolygon {
+            let rendrer = MKPolygonRenderer(overlay: overlay)
+            rendrer.fillColor = UIColor.black.withAlphaComponent(0.5)
+            rendrer.strokeColor = UIColor.orange
+            rendrer.lineWidth = 2
+            return rendrer
+        }
+        return MKOverlayRenderer()
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let annotation = view.annotation as? Place, let title = annotation.title else {
+            return
+        }
+        
+        let alertController = UIAlertController(title: "Welcome to \(title)", message: "Have a good time in \(title)", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
 
